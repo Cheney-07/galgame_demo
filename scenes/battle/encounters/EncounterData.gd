ef859b45121c50381063693f4960b7711495e3bc
Cheney-07 <1971561831@qq.com>
2026-07-27 15:05:59 +0800

    feat: add waves & max_enemies_on_field fields to EncounterData

--- scenes/battle/encounters/EncounterData.gd
+++ scenes/battle/encounters/EncounterData.gd
@@ -6,6 +6,8 @@ class_name EncounterData extends Resource
 @export var background_path: String = "res://images/battle_bg.png"
 @export var music_path: String = ""
 @export var player_bg_path: String = ""
-@export var enemy_ids: Array[String] = []        # enemy_id list
-@export var enemy_counts: Array[int] = []         # corresponding counts
+@export var enemy_ids: Array[String] = []        # [废弃] 使用 waves 替代
+@export var enemy_counts: Array[int] = []         # [废弃] 使用 waves 替代
 @export var is_boss: bool = false
+@export var waves: Array[WaveData] = []           # 波次列表（新）
+@export var max_enemies_on_field: int = 4         # 场上同时最多敌人数量
